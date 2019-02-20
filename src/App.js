39866b58358579ef8b9c0896978ace714bd985d3
bryanmartirez <bryan@fiddle.io>
2019-02-21 06:39:57 +0800

    init commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,93 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ApolloClient, { gql } from 'apollo-boost';
+import { ApolloProvider, Query, graphql } from 'react-apollo';
+import sample from 'lodash.sample';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const client = new ApolloClient({
+  uri: 'https://graphql-pokemon.now.sh',
+  clientState: {
+    defaults: { counter: 0 },
+    resolvers: {
+      Mutation: {
+        incCounter: (_, { value }, { cache }) => {
+          cache.writeData({ data: { counter: value } });
+          return null;
+        },
+      },
+    },
+  },
+});
+
+const GET_POKEMON = gql`
+  query($name: String!) {
+    counter @client
+    pokemon(name: $name) {
+      image
+    }
   }
-}
+`;
 
-export default App;
+const INCREMENT_COUNTER = gql`
+  mutation($value: String!) {
+    incCounter(value: $value) @client
+  }
+`;
+
+const pokemonArray = [
+  'Bulbasaur',
+  'Charmander',
+  'Ivysaur',
+  'Pikachu',
+  'Venusaur',
+];
+
+const Pokemon = ({ name, image }) => (
+  <div>
+    <img alt="pokemon" src={image} style={{ height: 200 }} />
+  </div>
+);
+
+let PokemonQuery = props => (
+  <Query query={GET_POKEMON} variables={{ name: sample(pokemonArray) }}>
+    {({ loading, error, data, refetch }) => {
+      if (error) return <div>Error</div>;
+
+      return (
+        <div style={{ textAlign: 'center', height: 200 }}>
+          {console.log('propshere', props, data.counter)}
+          {!loading ? (
+            <Pokemon name={data.pokemon.name} image={data.pokemon.image} />
+          ) : (
+            <div>
+              <img
+                alt="loading"
+                src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/0.16.1/images/loader-large.gif"
+                style={{ height: 200 }}
+              />
+            </div>
+          )}
+          <button
+            className="button is-primary"
+            onClick={async () => {
+              await refetch({ name: sample(pokemonArray) });
+              props.incCounter({ variables: { value: data.counter + 1 } });
+            }}
+            style={{ marginTop: 20 }}
+          >
+            Randomize - {data.counter}
+          </button>
+        </div>
+      );
+    }}
+  </Query>
+);
+
+const ApolloApp = () => (
+  <ApolloProvider client={client}>
+    <PokemonQuery />
+  </ApolloProvider>
+);
+
+PokemonQuery = graphql(INCREMENT_COUNTER, { name: 'incCounter' })(PokemonQuery);
+
+export default ApolloApp;
